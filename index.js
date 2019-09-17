652aff730e3c8f89a98b4e480c6b251affff96b9
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-09-16 17:25:11 -0700

    designed two new express routers to support REST API end points for promotions and leadership

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 const hostname = "localhost";
 const port = 3000;
 
@@ -12,32 +14,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use("/dishes", dishRouter); // In the Express App, any request coming with /dishes, will handle by dishRouter middleware.
-
-
-// server methods
-
-
-// // single dish
-// app.get("/dishes/:dishId", (req, res, next) => {
-//     res.end("Will send details of the dish: " + req.params.dishId + " to you!");
-// });
-//
-// app.post("/dishes/:dishId", (req, res, next) =>{
-//     res.statusCode = 403;
-//     res.end("POST operation not supported on /dishes/" + req.params.dishId);
-// });
-//
-// app.delete("/dishes/:dishId", (req, res, next) =>{
-//     res.end("Deleting dish: " + req.params.dishId);
-// });
-//
-// app.put("/dishes/:dishId", (req, res, next) =>{
-//     res.write("Updating the dish: " + req.params.dishId + " \n");
-//     res.end("Will update the dish: " + req.body.name + " with details: " + req.body.description);
-// });
-// //end of single dish
-// end of server methods
-
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 app.use((req, res, next) => {
     console.log(req.headers);
